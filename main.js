c42aaca4a258a1bc68bf6880ab3a6d1cbaf93b6b
Diarcip <diegocifuentes98@hotmail.com>
2020-07-03 15:16:46 -0500

    Se reduce el tiempo de las transiciones en el scroll

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
 });
 
 ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+ScrollReveal().reveal('.news-cards', { delay: 50 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 50 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 50 });
 
